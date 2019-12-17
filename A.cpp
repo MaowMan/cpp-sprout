32981c450fb149237a1cde26dce794ba74c8ebcc
MaowMan <54308739+MaowMan@users.noreply.github.com>
2019-12-17 23:45:18 +0800

    Create A.cpp

---
+++ A.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <cstdio>
+
+int main(){
+    int n;
+    std::cin>>n;
+    if (n==2){
+        int nxt;
+        std::cin>>nxt;
+        if (nxt==1){
+            putchar('2');
+            putchar('\n')
+        }
+        else{
+            putchar('1');
+            putchar('\n')
+        }
+    }
+    else if (n==3){
+
+    }
+}
