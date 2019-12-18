044dd0d8688d1bb9160ccf6ac609c0fda92cc9e3
MaowMan <54308739+MaowMan@users.noreply.github.com>
2019-12-18 15:27:02 +0800

    Update A.cpp

--- A.cpp
+++ A.cpp
@@ -2,11 +2,12 @@
 #include <vector>
 #include <cstdio>
 #include <set>
+
 int main(){
-    int n;
+    long long n;
     std::cin>>n;
     if (n==2){
-        int nxt;
+        long long nxt;
         std::cin>>nxt;
         if (nxt==1){
             putchar('2');
@@ -18,30 +19,74 @@ int main(){
         }
     }
     else if (n==3){
-        int a,b;
+        long long a,b;
         std::cin>>a>>b;
         if (a-(b-a)>0){
                 std::cout<<a-(b-a)<<std::endl;
         }
         else{
-            if((a+b)%2==0){
-                std::cout<<(a+b)/2<<std::endl;
+            if((b-a)%2==0){
+                std::cout<<a+(b-a)/2<<std::endl;
             }
             else{
                 std::cout<<b+(b-a)<<std::endl;
             }
         }
     }
+    else if(n==4){
+        long long a,b,c;
+        std::cin>>a>>b>>c;
+        if((b-a)==(c-b)){
+            if (a-(b-a)>0){
+                std::cout<<a-(b-a)<<std::endl;
+            }
+            else{
+                std::cout<<c+(b-a)<<std::endl;
+            }
+        }
+        else{
+            if((b-a)>(c-b)){
+                std::cout<<a+(b-a)/2<<std::endl;
+            }
+            else{
+                std::cout<<b+(c-b)/2<<std::endl;
+            }
+        }
+    }
     else{
-        int prev,next,cache,first,last;
-        std::cin>>prev>>next;
-        frist=prev;
-        log=next-prev;
-        log.insert(next-prev)
-        while(std::cin>>cache){
-            prev=next;
-            next=cache;
-            if ()
+        long long a,b,c,d;
+        std::cin>>a>>b>>c>>d;
+        if((b-a)==(c-b)==(d-c)){
+            long long log=(b-a);
+            long long prev,next,cache;
+            prev=c;
+            next=d;
+            while(std::cin>>cache){
+                prev=next;
+                next=cache;
+                if ((next-prev)!=log){
+                    std::cout<<prev+(next-prev)/2<<std::endl;
+                    return 0;
+                }
+            }
+            if ((a-log)>0){
+                std::cout<<(a-log)<<std::endl;
+            }
+            else{
+                std::cout<<(next+log)<<std::endl;
+            }
+        }
+        else{
+            if((b-a)==(c-b)){
+                std::cout<<c+(d-c)/2<<std::endl;
+            }
+            else if((b-a)==(d-c)){
+                std::cout<<b+(c-b)/2<<std::endl;
+            }
+            else{
+                std::cout<<a+(b-a)/2<<std::endl;
+            }
+            return 0;
         }
     }
 }
