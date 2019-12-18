de732816b1ce88fac63035d3facd57120f9cf2be
MaowMan <54308739+MaowMan@users.noreply.github.com>
2019-12-18 12:55:23 +0800

    Update A.cpp

--- A.cpp
+++ A.cpp
@@ -10,14 +10,26 @@ int main(){
         std::cin>>nxt;
         if (nxt==1){
             putchar('2');
-            putchar('\n')
+            putchar('\n');
         }
         else{
             putchar('1');
-            putchar('\n')
+            putchar('\n');
         }
     }
     else if (n==3){
-
+        int a,b;
+        std::cin>>a>>b;
+        if (a-(b-a)>0){
+                std::cout<<a-(b-a)<<std::endl;
+        }
+        else{
+            if((a+b)%2==0){
+                std::cout<<(a+b)/2<<std::endl;
+            }
+            else{
+                std::cout<<b+(b-a)<<std::endl;
+            }
+        }
     }
 }
