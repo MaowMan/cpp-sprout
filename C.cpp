e16e439260bd284418e009a8e2e121945de60aaf
MaowMan <54308739+MaowMan@users.noreply.github.com>
2019-12-19 21:47:06 +0800

    Update C.cpp

--- C.cpp
+++ C.cpp
@@ -3,6 +3,13 @@
 #include <iostream>
 #include <string>
 
+void print(std::vector<int> &data){
+    std::cout<<data.size();
+    for(int i=0;i<data.size();i++){
+        std::cout<<' '<<data[i];
+    }
+    std::cout<<std::endl;
+}
 std::vector<int> match(std::vector<int> &data){
     std::vector<int> res;
     for(int i=0;i<data.size();i++){
@@ -18,19 +25,15 @@ std::vector<int> match(std::vector<int> &data){
     return res;
 }
 
-std::string print(std::vector<int> &data){
-    std::string res;
-    res+=std::to_string(data.size());
-    for(int i=0;i<data.size();i++){
-        res+=" ";
-        res+=std::to_string(data[i]);
-    }
-    res+="\n";
-    return res;
-}
 int main(){
     int na,nb;
     std::cin>>na>>nb;
+    if (na==0||nb==0){
+        for(int i=0;i<3;i++){
+            std::cout<<0<<std::endl;
+        }
+        return 0;
+    }
     std::vector<int> a(na),b(nb);
     for(int i=0;i<na;i++){
         std::cin>>a[i];
@@ -39,8 +42,8 @@ int main(){
         std::cin>>b[i];
     }
     int limit;
-    int turn;
-    std::string res;
+    int turn=-1;
+    std::vector<std::vector<int>> res;
     std::cin>>limit;
     for(int i=0;i<limit;i++){
         int ra,rb;
@@ -49,27 +52,33 @@ int main(){
         if(ptr_ra!=b.end()){
             b.erase(ptr_ra);
             a.push_back(ra);
-            a=match(a);
-        }
-        std::cout<<print(a);
-        res+=print(a);
-        if (a.size()==0){
-            turn=i+1;
-            break;
         }
         std::vector<int>::iterator ptr_rb=find(a.begin(),a.end(),rb);
         if(ptr_rb!=a.end()){
             a.erase(ptr_rb);
             b.push_back(rb);
-            b=match(b);
         }
-        std::cout<<print(a);
-        res+=print(b);
-        if (b.size()==0){
+        //print(a);
+        //print(b);
+        a=match(a);
+        b=match(b);
+        res.push_back(a);
+        res.push_back(b);
+        if (a.size()==0||b.size()==0){
             turn=i+1;
             break;
         }
     }
+    if (turn==-1){
+        turn=limit;
+    }
     std::cout<<turn<<std::endl;
-    std::cout<<res<<std::endl;
+    for (int i=0;i<res.size();i++){
+        std::cout<<res[i].size();
+        for(int k=0;k<res[i].size();k++){
+            std::cout<<' '<<res[i][k];
+        }
+        std::cout<<'\n';
+    }
+    return 0;
 }
