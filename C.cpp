59c44826d446bb12fc41f95195b96382a18bdc73
MaowMan <54308739+MaowMan@users.noreply.github.com>
2019-12-19 17:03:16 +0800

    Update C.cpp

--- C.cpp
+++ C.cpp
@@ -1,66 +1,75 @@
-#include <iostream>
 #include <vector>
 #include <algorithm>
+#include <iostream>
+#include <string>
 
-void print(std::vector<int> &vec){
-    std::cout<<vec.size();
-    for(int i=0;i<vec.size();i++){
-        std::cout<<' '<<vec[i];
-    }
-    std::cout<<std::endl;
-}
-
-void match(std::vector<int> &vec){
-    std::cout<<"matching"<<std::endl;
-    print(vec);
-    for(std::vector<int>::iterator i=vec.begin();i!=vec.end();++i){
-        std::vector<int>::iterator ptr=find(vec.begin(),vec.end(),*i);
-        if (ptr==vec.end()){
-            std::cout<<"continue"<<std::endl;
-            continue;
+std::vector<int> match(std::vector<int> &data){
+    std::vector<int> res;
+    for(int i=0;i<data.size();i++){
+        std::vector<int>::iterator ptr;
+        ptr=find(res.begin(),res.end(),data[i]);
+        if(ptr==res.end()){
+            res.push_back(data[i]);
         }
         else{
-            vec.erase(i);
-            vec.erase(ptr);
-            std::cout<<"erasing"<<std::endl;
-            print(vec);
+            res.erase(ptr);
+        }
+    }
+    return res;
+}
+
+std::string print(std::vector<int> &data){
+    std::string res;
+    res+=std::to_string(data.size());
+    for(int i=0;i<data.size();i++){
+        res+=" ";
+        res+=std::to_string(data[i]);
+    }
+    res+="\n";
+    return res;
+}
+int main(){
+    int na,nb;
+    std::cin>>na>>nb;
+    std::vector<int> a(na),b(nb);
+    for(int i=0;i<na;i++){
+        std::cin>>a[i];
+    }
+    for(int i=0;i<nb;i++){
+        std::cin>>b[i];
+    }
+    int limit;
+    int turn;
+    std::string res;
+    std::cin>>limit;
+    for(int i=0;i<limit;i++){
+        int ra,rb;
+        std::cin>>ra>>rb;
+        std::vector<int>::iterator ptr_ra=find(b.begin(),b.end(),ra);
+        if(ptr_ra!=b.end()){
+            b.erase(ptr_ra);
+            a.push_back(ra);
+            a=match(a);
+        }
+        std::cout<<print(a);
+        res+=print(a);
+        if (a.size()==0){
+            turn=i+1;
+            break;
+        }
+        std::vector<int>::iterator ptr_rb=find(a.begin(),a.end(),rb);
+        if(ptr_rb!=a.end()){
+            a.erase(ptr_rb);
+            b.push_back(rb);
+            b=match(b);
+        }
+        std::cout<<print(a);
+        res+=print(b);
+        if (b.size()==0){
+            turn=i+1;
             break;
         }
     }
+    std::cout<<turn<<std::endl;
+    std::cout<<res<<std::endl;
 }
-
-int main(){
-    int a,b;
-    std::cin>>a>>b;
-    std::vector<int> A(a),B(b);
-    int cache;
-    for(int i=0;i<a;i++){
-        std::cin>>cache;
-        A[i]=cache;
-    }
-    for(int i=0;i<b;i++){
-        std::cin>>cache;
-        B[i]=cache;
-    }
-    int k,x,y;
-    std::cin>>k;
-    for (int i=0;i<k;i++){
-        std::cin>>x>>y;
-        std::vector<int>::iterator iB=find(B.begin(),B.end(),x);
-        if (iB!=B.end()){
-            A.push_back(x);
-            B.erase(iB);
-            match(A);
-        }
-        std::vector<int>::iterator iA=find(A.begin(),A.end(),y);
-        if (iA!=B.end()){
-            B.push_back(y);
-            A.erase(iA);
-            match(B);
-        }
-        print(A);
-        print(B);
-    }
-}
-
-
