bc1e1fc91194e10922924fb85395c7d3bff00206
MaowMan <54308739+MaowMan@users.noreply.github.com>
2019-12-19 22:54:04 +0800

    Update B.cpp

--- B.cpp
+++ B.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
-#include <algorithm>
-inline long long cal(long long a,long long b,long long c,long k){
+
+long long min(long long a,long long b){
+    if(a>=b){
+        return b;
+    }
+    else{
+        return a;
+    }
+}
+inline long long cal(long long a,long long b,long long c,long long k){
     return a*(k*k)+b*(k)+c;
 }
-
 int main(){
     long long a,b,c;
     std::cin>>a>>b>>c;
@@ -13,37 +20,26 @@ int main(){
     std::cin>>n;
     std::vector<long long> data;
     for(int k=-50;k<51;k++){
-        long long cache=cal(a,b,c,k);
-        //std::cout<<cache<<std::endl;
-        data.push_back(cache);
+            long long cache=cal(a,b,c,k);
+            //std::cout<<cache<<std::endl;
+            data.push_back(cache);
     }
-    std::sort(data.begin(),data.end());
     for(int i=0;i<n;i++){
-        int l,r,ll,lr;
+        long long l,r,result=9999999999;
         std::cin>>l>>r;
-        long long result=9223372036854775800;
         for(int k=0;k<data.size();k++){
-            if (data[k]>=l){
-                ll=k-1;
+            if(data[k]>=l&&data[k]<=r){
+                result=0;
                 break;
             }
-        }
-        for(int k=data.size()-1;k>=0;k--){
-            if(data[k]<=r){
-                lr=k+1;
-                break;
-            }
-        }
-        std::cout<<"ll:"<<ll<<"lr"<<lr<<std::endl;
-        for(int k=l;k<r;k++){
-            for(int p=ll;p<=lr;p++){
-                long long cache=std::abs(data[p]-k);
-                std::cout<<cache;
-                if(cache<result){
+            else{
+                long long cache=min(std::abs(data[k]-l),std::abs(data[k]-r));
+                //std::cout<<"cache:"<<cache<<"result:"<<result<<std::endl;
+                if (cache<=result){
                     result=cache;
                 }
             }
         }
-        std::cout<<result<<'\n';
+        std::cout<<result<<std::endl;
     }
 }
